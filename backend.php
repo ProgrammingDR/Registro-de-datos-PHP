dc84a7b155dd862a4b86994c49b3d8e51256110a
PrograminDR <96454918+PrograminDR@users.noreply.github.com>
2022-02-12 11:57:29 -0400

    1

--- backend.php
+++ backend.php
@@ -10,12 +10,6 @@
        public $imp;
        
 
-      
-        
-           
-        
-           
-
         public function GuardarDatos()
         {
             $ID = $_REQUEST['ID'];
@@ -24,28 +18,35 @@
             $car = $_REQUEST['cargo'];
             $suel = $_REQUEST['sueldo']; 
             
-            
-           
-            
             $ar = fopen("datos.txt", "a") or die("Problemas en la creacion");
+            fputs($ar, "ID: ");
             fputs($ar, $ID);
             fputs($ar,"\n");
+            fputs($ar, "Nombre: ");
             fputs($ar, $Nom);
             fputs($ar,"\n");
+            fputs($ar, "Apellido: ");
             fputs($ar, $ap);
             fputs($ar,"\n");
+            fputs($ar, "Cargo: ");
             fputs($ar, $car);
             fputs($ar,"\n");
+            fputs($ar, "Sueldo: ");
             fputs($ar, $suel);
             fputs($ar,"\n");
+
             if ($suel >= 15000) {
                 $imp = $suel * 0.05;
+                fputs($ar, "Impuesto: ");
                 fputs($ar, $imp); 
             }else{
                 $imp = 0;
+                fputs($ar, "Impuesto: ");
                 fputs($ar, $imp); 
             }
+
             fputs($ar,"\n");
+            fputs($ar, "<hr>");
             fputs($ar,"\n");
             
             fclose($ar);
