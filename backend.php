42459168bf489b9f65aa7993be9c12a49288b0eb
PrograminDR <96454918+PrograminDR@users.noreply.github.com>
2022-02-11 22:17:04 -0400

    initial commit

---
+++ backend.php
@@ -0,0 +1,56 @@
+<?php
+   class Guardar 
+   {
+       public $ar;
+       public $ID;
+       public $Nom;
+       public $ap;
+       public $car;
+       public $suel;
+       public $imp;
+       
+
+      
+        
+           
+        
+           
+
+        public function GuardarDatos()
+        {
+            $ID = $_REQUEST['ID'];
+            $Nom = $_REQUEST['Nombre'];
+            $ap = $_REQUEST['apellido'];
+            $car = $_REQUEST['cargo'];
+            $suel = $_REQUEST['sueldo']; 
+            
+            
+           
+            
+            $ar = fopen("datos.txt", "a") or die("Problemas en la creacion");
+            fputs($ar, $ID);
+            fputs($ar,"\n");
+            fputs($ar, $Nom);
+            fputs($ar,"\n");
+            fputs($ar, $ap);
+            fputs($ar,"\n");
+            fputs($ar, $car);
+            fputs($ar,"\n");
+            fputs($ar, $suel);
+            fputs($ar,"\n");
+            if ($suel >= 15000) {
+                $imp = $suel * 0.05;
+                fputs($ar, $imp); 
+            }else{
+                $imp = 0;
+                fputs($ar, $imp); 
+            }
+            fputs($ar,"\n");
+            fputs($ar,"\n");
+            
+            fclose($ar);
+        }
+        
+   }
+   
+?>
